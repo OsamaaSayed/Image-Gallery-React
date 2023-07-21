9b585152e766a5751e7c2d8bb2ce78d25829ae20
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-07-21 22:58:11 +0300

    💄 Design Enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -66,15 +66,13 @@ function App() {
             <span className="loading loading-spinner loading-lg"></span>
           </div>
         )}
-        {images.length && <ImageList images={images} />}
-        {isError && (
-          <div className="flex justify-center items-center">
-            <img
+        {images.length && !isError ? <ImageList images={images} /> : <div className="flex justify-center items-center">
+            <img className="w-1/2"
               src="https://cdn.dribbble.com/users/256646/screenshots/17751098/media/768417cc4f382d6171053ad620bc3c3b.png?resize=1000x750&vertical=center"
               alt="No Data"
             />
-          </div>
-        )}
+          </div>}
+  
       </section>
     </>
   );
